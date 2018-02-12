398b8dc2d23eaba32c834ca390ff427abdead99c
Shintaro Masuno <shintaromasuno@gmail.com>
2018-02-12 13:21:20 -0600

    Update test_sparkr.R

--- test_sparkr.R
+++ test_sparkr.R
@@ -4,10 +4,5 @@
 library(sparklyr)
 library(dplyr)
 
-Sys.setenv(AWS_ACCESS_KEY_ID="AKIAJP22Q4FVLLRMTMGA")
-Sys.setenv(AWS_SECRET_ACCESS_KEY="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")
 
-conf <- spark_config()
-conf$sparklyr.defaultPackages<-'org.apache.hadoop:hadoop-aws:2.8.2'
-conf$spark.executormemory<-'14g'
 
